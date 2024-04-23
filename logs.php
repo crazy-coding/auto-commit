34f2b3717e73630b311808500442f445ab0b21db
crazy-coding <trulydev122@gmail.com>
2024-04-23 15:41:26 +0000

    logs table

--- logs.php
+++ logs.php
@@ -59,6 +59,13 @@ ob_start();
       </div>
     </div>
   </div>
+
+  <!-- Page level plugins -->
+  <script src="assets/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="assets/js/demo/datatables-demo.js"></script>
 </div>
 <?php
 // Set content file
